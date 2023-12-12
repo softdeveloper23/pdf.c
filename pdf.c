0ff96cf9561139b483df7667d36e0f4fb7ced4e8
unknown <brannongarrett77@gmail.com>
2023-12-12 18:12:31 +0100

    Updated the code to account for not being a PDF file

--- pdf.c
+++ pdf.c
@@ -20,11 +20,22 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    // Read 4 bytes from file
+    // An array with 4 bytes to store the first 4 bytes of the file
     uint8_t buffer[4];
+
+    // An array with integers representing the PDF signature
+    uint8_t signature[] = {37, 80, 68, 70};
+
     fread(buffer, 1, 4, file); // Read into buffer, 1 at a time, 4 bytes total, from file pointer
     for (int i = 0; i < 4; i++)
     {
+        if (buffer[i] != signature[i])
+        {
+            printf("Not likely a PDF file!\n");
+            return 0;
+        }
         printf("%i ", buffer[i]);
     }
+    printf("\nLikely a PDF file!\n");
+    return 0;
 }
