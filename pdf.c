ac0ca4c6698c8fe59d5f816b3e067b0068c5525f
unknown <brannongarrett77@gmail.com>
2023-12-12 17:48:20 +0100

    First commit

---
+++ pdf.c
@@ -0,0 +1,12 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // Ensure proper usage
+    if (argc != 2)
+    {
+        printf("Improper usage\n");
+        return 1;
+    }
+}
